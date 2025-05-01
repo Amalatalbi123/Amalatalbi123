6992aa6cff1ce24bb8bf73b7d395881c10ef6771
amalatalbi123 <talbiamal84@gmail.com>
2025-05-01 14:59:04 +0100

    first commit by me

---
+++ python.py
@@ -0,0 +1,9 @@
+# test_vscode.py
+
+def main():
+    print("Bienvenue dans VS Code avec Python !")
+    nom = input("Quel est votre nom ? ")
+    print(f"Bonjour, {nom} ! Ravi de vous voir coder avec Python.")
+
+if __name__ == "__main__":
+    main()
